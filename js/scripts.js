85c18d94b0ad940d1d0cfaf9fd4322d7917f77eb
Alexandr Mironenko <mnemonik900000@gmail.com>
2021-01-05 01:00:08 +0200

    Home Work js 2

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+let number = prompt('Enter the number from 0 to 9', 0);
+
+switch(number) {
+    case '0':
+        document.write('0 - )');
+        break;
+    case '1':
+        document.write('1 - !');
+        break;
+    case '2':
+        document.write('2 - @');
+        break;
+    case '3':
+        document.write('3 - #');
+        break;
+    case '4':
+        document.write('4 - $');
+        break;
+    case '5':
+        document.write('5 - %');
+        break;
+    case '6':
+        document.write('6 - ^');
+        break;
+    case '7':
+        document.write('7 - &');
+        break;
+    case '8':
+        document.write('8 - *');
+        break;
+    case '9':
+        document.write('9 - (');
+        break;
+}
